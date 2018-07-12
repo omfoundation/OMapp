5a2100d2add33e7bfa5dfc6b65ddc1074b0ea0ea
Madot <migueldeolim1@gmail.com>
2018-07-12 12:29:38 -0400

    Test button

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,22 +1,77 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import '../css/App.css';
 
 class LoginButton extends Component {
 
     constructor(props){
         super(props);
+        
+        this.state = {
+            user: null
+        };
 
         this.LogInClick = this.LogInClick.bind(this); 
-    }
+        //this.componentWillMount = this.componentWillMount.bind(this);
+        this.LogOutClick = this.LogOutClick.bind(this);
+
+    };
 
     LogInClick(){
-        alert('Iniciando...');
+        //alert('Iniciando...');
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+            //OK
+        }).catch(function(error) {
+            //Error 
+            console.log(error);
+        });
+    };
+
+    LogOutClick(){
+        //Cerrando sesion
+        firebase.auth().signOut().then(function() {
+            //OK
+            this.setState({ user: null });
+          }).catch(function(error) {
+            //ERROR
+            console.log(error);
+          });
+    }
+
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ user });
+            }else{
+                //this.setState({ user: null });
+            } 
+        });
     }
 
     render(){
-        return(
-            <button onClick={this.LogInClick}>Iniciar sesión con Google</button>
-        );
+        console.log('render '+ this.state.user);
+        if(this.state.user){
+            //Logeado
+            return(
+                <div>
+                    <p>
+                        Hola {this.state.user.displayName}!
+                    </p>
+                    <button onClick={this.LogOutClick}>Cerrar sesión</button>
+                </div>
+            );
+
+        }else{
+            //No hay datos de user/no logeado
+            return(
+                <button onClick={this.LogInClick}>Iniciar sesión con Google</button>
+            );
+        }
+        
     }
 }
 
