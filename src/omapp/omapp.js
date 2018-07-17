c3d26b3b14181a5d50c63aa8989be7d5b5a12de6
Luis Alejandro Morin <luis.morin@gmail.com>
2018-07-17 08:51:28 -0400

    .

--- src/omapp/omapp.js
+++ src/omapp/omapp.js
@@ -1,16 +1,19 @@
 //import firebase from 'firebase';
 
-import firebase, {auth, provider} from '../firebase';
+import firebase from '../firebase';
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
 
 var omapp = {
 
 	signInWithPopup : function(state){
-        auth.signInWithPopup(firebase.provider)
+        auth.signInWithPopup(provider)
             .then((result) => {
                 //OK
                 //console.log(result);
                 const user = result.user;
-                state.setState({
+                this.setState({
                     user: user
                 });
             }).catch(function(error) {
