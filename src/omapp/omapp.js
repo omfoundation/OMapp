d52f669d14975cbe0d0ff0ab4cb23f9df05bf51b
Luis Alejandro Morin <luis.morin@gmail.com>
2018-07-17 08:43:40 -0400

    .

--- src/omapp/omapp.js
+++ src/omapp/omapp.js
@@ -1,8 +1,6 @@
 //import firebase from 'firebase';
 
 import firebase from 'firebase';
-import auth from 'firebase';
-import provider from 'firebase';
 
 //inicializamos configuracion de firebase con datos de omapp-2018
 firebase.initializeApp({
@@ -17,18 +15,18 @@ firebase.initializeApp({
 var omapp = {
 
 	signInWithPopup : function(state){
-		firebase.auth().signInWithPopup(provider)
-			.then((result) => {
-				//OK
-				//console.log(result);
-				alert('then');
-				state.setState({
-					//user: user
-				});
-			}).catch(function(error) {
-				//Error 
-				console.log(error);
-		});
+        firebase.auth.signInWithPopup(firebase.provider)
+            .then((result) => {
+                //OK
+                //console.log(result);
+                const user = result.user;
+                state.setState({
+                    user: user
+                });
+            }).catch(function(error) {
+                //Error 
+                console.log(error);
+        });
 	},
 	
 	onMount : function(state){
