09d6f3614a733a16727250d0396de02a9e0b5577
Luis Alejandro Morin <luis.morin@gmail.com>
2018-08-07 23:38:58 -0400

    Pequena modificacion para desplazar la foto del usuario por defecto

--- src/omapp/omapp.js
+++ src/omapp/omapp.js
@@ -11,72 +11,19 @@ db.settings(settings);
 
 
 var omapp = {
-    defaultPhotoURL : 'http://sitelcity.com/wp-content/uploads/2015/04/default-user-image-300x300.png',
-    
+
     dataUser: {
         user: null,
         inDB: null,
         style: null,
         nick: null,
         email: null,
-        photoURL: null
+        photoURL: null,
+        defaultPhotoURL : 'http://sitelcity.com/wp-content/uploads/2015/04/default-user-image-300x300.png'
     },
 
     setLogStyle : function(style){
         this.dataUser.style = style;
-    },
-
-    checkReg : function(comp) {
-        let user = omapp.dataUser.user;
-
-            //verificamos si el user esta registrado en la db
-            if(user){
-                let email = user.email;
-                console.log('email:' + email);
-        
-                let userDB = db.collection("users");
-                let userRef = userDB.doc(email);
-    
-                userRef.get().then(function(doc) {
-                
-                    if (doc.exists) {
-                    //Usuario existe
-                    console.log("Document data:", doc.data());
-                    console.log('omapp.checkReg - Usuario existe en db');
-
-                    omapp.dataUser.inDB = true;
-                    omapp.dataUser.nick = doc.data().nick;
-                    omapp.dataUser.email = email;
-
-                    let sta = comp.state;
-                    sta.loader = false;
-
-                    comp.setState(sta);
-                
-                } else {
-                    // doc.data() will be undefined in this case
-                    //console.log("No such document!");
-                    console.log('Usuario NO existe en db2');
-                    omapp.dataUser.inDB = false;
-
-                    let sta = comp.state;
-                    sta.loader = false;
-
-                    comp.setState(sta);
-
-                }
-            }).catch(function(error) {
-                //No existe user
-                console.log("Error getting document:", error);
-                console.log('Usuario NO existe en db ');
-                
-                omapp.dataUser.inDB = false;
-
-
-            });
-                
-            }
-
     },
 
 	signInWithGoogle : function(component){
@@ -134,26 +81,6 @@ var omapp = {
         });
     },
 
-    signOut : function(component){
-        auth.signOut()
-            .then(() => {
-                omapp.dataUser = {
-                    user: null,
-                    inDB: null
-                };
-
-                let sta = component.state;
-                sta.user = null;
-                sta.inDB = null;
-                sta.loginStatus = "NOT_AUTHENTICATED";
-
-                component.setState(sta);
-
-        }).catch(function(error) {
-            console.log(error);
-        });
-    },
-
     signOutPromise : function(){
         return new Promise(function(resolve, reject){
             auth.signOut()
@@ -172,41 +99,6 @@ var omapp = {
         })
     },
 	
-	onMount : function(component){
-		firebase.auth().onAuthStateChanged((user) => {
-			if (user) {
-				component.setState({
-                    user: user,
-                    inDB: omapp.getUser(user)
-                });
-			} 
-		});	
-	},
-
-    getCurrentuser :  function(){
-        //console.log(firebase.auth().currentUser);
-        return firebase.auth().currentUser;
-
-    },
-
-    RegDB(email,doc,nick){
-        //REGISTRAMOS EN NUESTRA BD
-
-
-
-        db.collection('users').doc(email).set(doc)
-            .then(function() {
-                console.log("Document successfully written!");
-                omapp.dataUser.inDB = true;
-                omapp.dataUser.nick = nick;
-                omapp.dataUser.email = email;
-        }).catch(function(error) {
-                console.log("Registro incompleto");
-                console.log(error);
-                throw error;
-        });
-    },
-
     RegDBPromise(email,doc,nick){
         
         //REGISTRAMOS EN NUESTRA BD
@@ -278,42 +170,6 @@ var omapp = {
                 }
             }
         );
-
-    },
-
-    completeRegDB: function(email, pass, nick, idPlan, authLevel){
-        
-        let fch = new Date();
-        
-        let docData = {
-            type: omapp.dataUser.style,
-            FechaUnion: fch,
-            nick: nick,
-            plan: idPlan,
-            authLevel: authLevel
-        }
-
-        if(omapp.dataUser.style == 'g'){
-            //Registro mediante google
-            email = omapp.dataUser.user.email;
-
-            omapp.RegDB(email, docData, nick);
-        }else{
-            //Registro por email
-            //REGISTRAMOS CON FIREBASE
-            auth.createUserAndRetrieveDataWithEmailAndPassword(email, pass).
-                then(function(u){
-                    console.log('Logrado', u);
-                    omapp.dataUser.user = u;
-                    omapp.RegDB(email, docData, nick);
-                })
-                .catch(function(error) {
-                    // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.log(errorCode, errorMessage);
-                });
-        }
     }
 }
 
