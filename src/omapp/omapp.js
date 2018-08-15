1a5eb70ac3ae2a5b39082668522a3d62ff22d9d0
lamorin <luis.morin@gmail.com>
2018-08-15 15:22:19 -0400

    Update omapp.js
    Cambios cosméticos

--- src/omapp/omapp.js
+++ src/omapp/omapp.js
@@ -12,57 +12,57 @@ db.settings(settings);
 export function signInWithGooglePromise() {
     return new Promise(function(resolve, reject) {
         auth.signInWithPopup(provider)
-            .then((result) => {
-                db.collection('users').doc(result.user.email).get()
-                    .then((doc) => {
-                        resolve({
-                            email: result.user.email,
-                            username: doc.username,
-                            signupMethod: doc.signupMethod,
-                            registered: true,
-                            profilePhotoURL: doc.profilePhotoURL
-                        })
-                    });
-            }).catch((error) => {
-                console.log(error);
-                reject(error);
-            });
+        .then((result) => {
+            db.collection('users').doc(result.user.email).get()
+                .then((doc) => {
+                    resolve({
+                        email: result.user.email,
+                        username: doc.username,
+                        signupMethod: doc.signupMethod,
+                        registered: true,
+                        profilePhotoURL: doc.profilePhotoURL
+                    })
+                });
+        }).catch((error) => {
+            console.log(error);
+            reject(error);
+        });
     });
 }
 
 export function signInWithEmailPromise(email, password) {
     return new Promise(function(resolve, reject) {
         auth.signInAndRetrieveDataWithEmailAndPassword(email, password)
-            .then(function(result) {
-                console.log(result);
-                db.collection('users').doc(result.user.email).get()
-                    .then((doc) => {
-                        resolve({
-                            email: email,
-                            nickname: doc.nickname,
-                            signupMethod: doc.signupMethod,
-                            registered: true,
-                            profilePhotoURL: doc.profilePhotoURL
-                        })
-                    })
-                    .catch((error) => console.log(error));
+        .then(function(result) {
+            console.log(result);
+            db.collection('users').doc(result.user.email).get()
+            .then((doc) => {
+                resolve({
+                    email: email,
+                    nickname: doc.nickname,
+                    signupMethod: doc.signupMethod,
+                    registered: true,
+                    profilePhotoURL: doc.profilePhotoURL
+                })
             })
-            .catch(function(error) {
-                console.log(error);
-                reject(error);
-            });
+            .catch((error) => console.log(error));
+        })
+        .catch(function(error) {
+            console.log(error);
+            reject(error);
+        });
     });
 }
 
 export function signOutPromise() {
     return new Promise(function(resolve, reject) {
         auth.signOut()
-            .then(() => {
-                resolve();
-            }).catch(function(error) {
-                console.log(error);
-                reject(error);
-            });
+        .then(() => {
+            resolve();
+        }).catch(function(error) {
+            console.log(error);
+            reject(error);
+        });
     });
 }
 
@@ -72,39 +72,39 @@ export function completeRegDBPromise(user, password) {
             user.signupDate = new Date();
             if (user.signupMethod === 'google.com') {
                 auth.signInWithPopup(provider)
-                    .then((result) => {
-                        console.log(result);
-                        var email = result.user.email;
+                .then((result) => {
+                    console.log(result);
+                    var email = result.user.email;
 
-                        user.email = result.user.email;
+                    user.email = result.user.email;
 
-                        regDBPromise(user).
-                        then(function() {
-                            resolve(user);
-                        }).
-                        catch(function(error) {
-                            console.error(error)
-                            reject(error);
-                        });
-                    })
-                    .catch(function(error) {
-                        console.log(error);
+                    regDBPromise(user).
+                    then(function() {
+                        resolve(user);
+                    }).
+                    catch(function(error) {
+                        console.error(error)
                         reject(error);
                     });
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    reject(error);
+                });
             } else {
                 auth.createUserAndRetrieveDataWithEmailAndPassword(user.email, password)
                 .then(function() {
                     regDBPromise(user)
-                        .then(
-                            function(data) {
-                                console.log(data);
-                                resolve(user);
-                            }
-                        )
-                        .catch(function(error) {
-                            console.log(error);
-                            reject(error);
-                        });
+                    .then(
+                        function(data) {
+                            console.log(data);
+                            resolve(user);
+                        }
+                    )
+                    .catch(function(error) {
+                        console.log(error);
+                        reject(error);
+                    });
                 })
                 .catch(function(error) {
                     // Handle Errors here.
@@ -119,11 +119,11 @@ export function completeRegDBPromise(user, password) {
 function regDBPromise(user) {
     return new Promise(function(resolve, reject) {
         db.collection('users').doc(user.email).set(user)
-            .then(function(data) {
-                resolve(user);
-            }).catch(function(error) {
-                console.log(error);
-                reject(error);
-            });
+        .then(function(data) {
+            resolve(user);
+        }).catch(function(error) {
+            console.log(error);
+            reject(error);
+        });
     });
 }
