1eac187c565069f78e34e1fc24f6625aa6ac1167
lamorin <luis.morin@gmail.com>
2018-08-31 13:56:09 -0400

    Centrado del contenedor de botones de Login y Registro

--- src/components/Access.react.js
+++ src/components/Access.react.js
@@ -8,12 +8,12 @@ export default class Access extends Component {
 
     render() {
         return (
-            <Grid centered verticalAlign='middle' columns={2}>
-                <Grid.Row >
-                    <Grid.Column>
+            <Grid verticalAlign='middle' columns={2}>
+                <Grid.Row centered>
+                    <Grid.Column width={8}>
                         <br />
                         <br />
-                        <Card >
+                        <Card fluid>
                             <Card.Content>
                                 <Image floated='right' size='mini' alt="logo" src='favicon.ico' />
                                 <Card.Header>OMapp</Card.Header>
@@ -75,103 +75,4 @@ export default class Access extends Component {
         )
     }
 
-}
-
-class SignupMethodChoicePopup extends Component {
-
-    onSignupWithEmailAndPasswordHandler() {
-        this.props.signupWithEmailPasswordHandler();
-    }
-
-    closeClickHandler() {
-        this.props.onCloseHandler();
-    }
-
-    signupWithEmailPasswordClickHandler() {
-        this.props.onSignupWithEmailAndPasswordHandler();
-    }
-
-    signupWithGoogleClickHandler() {
-        this.props.onSignupWithGoogleHandler();
-    }
-
-    render() {
-        return (
-            <div ref="modRes" className="modal" style={this.props.style}>
-                <div className="modal-content">
-                    <span className="close" onClick={this.closeClickHandler.bind(this)}>&times;</span>
-                    <h2>Registro</h2>
-                    <p>Selecciona tu metodo preferido:</p>
-                    <a className="btn"><button onClick={() => { this.signupWithEmailPasswordClickHandler() }}>Con email y contraseña</button></a>
-                    <br />
-                    <button onClick={() => this.signupWithGoogleClickHandler()}>Con cuenta Google</button>
-                </div>
-            </div>
-
-        )
-    }
-}
-
-class LoginMethodPopup extends Component {
-
-    onLoginWithGoogle() {
-        this.props.googleAuthenticationHandler();
-    }
-
-    checkLogData() {
-        let txtEmail = this.refs.uemail.value;
-        let mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/;
-
-        if ((txtEmail.replace(/\s/g, '') !== "") && (txtEmail.match(mailformat))) {
-            //Chequeamos que no sea solo blancos
-            //ok
-            //return true;
-
-            let txtPas = this.refs.upsw.value;
-
-            if (txtPas.replace(/\s/g, '') !== "") {
-                //Nada de blancos 
-                //OK
-            } else {
-                alert("Introduce una clave valida");
-                return false;
-            }
-
-        } else {
-            alert("Introduce un email valido!");
-            return false;
-        }
-
-        return true;
-    }
-
-    onProcessLogin() {
-        if (this.checkLogData()) {
-            this.props.processLoginHandler(this.refs.uemail.value, this.refs.upsw.value);
-        }
-    }
-
-    onClose() {
-        this.props.onCloseHandler();
-    }
-
-    render() {
-        return (
-            <div ref="modLogIn" className="modal" style={this.props.style}>
-                {/* Modal Content */}
-                <div className="modal-content animate">
-                    <span className="close red" onClick={this.onClose.bind(this)}>&times;</span>
-                    <div className="container">
-                        <label htmlFor="uemail"><b>Email</b></label>
-                        <input type="text" placeholder="Enter email" ref="uemail" required />
-                        <label htmlFor="upsw"><b>Contraseña</b></label>
-                        <input type="password" placeholder="Enter contraseña" ref="upsw" required />
-                        <button type="submit" className="btn greenBG" onClick={this.onProcessLogin.bind(this)}>Iniciar sesion</button>
-                        <br />
-                        <button className={"entra con google"} onClick={() => this.onLoginWithGoogle()}>Entra con Google</button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
 }
