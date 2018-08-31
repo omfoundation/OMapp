777a4a1f9b1303d2d9c71a54e782de838a35d7de
Madot <migueldeolim1@gmail.com>
2018-08-31 16:00:05 -0400

    SignUp-> +Semantic UI

--- src/components/Signup.react.js
+++ src/components/Signup.react.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { User } from '../omapp/omapp'
 
-export default class SignUp extends Component{
+import { Form, Grid, Card, Button, List, Icon } from 'semantic-ui-react';
 
-    completarReg(idPlan, authLevel){
-        if(this.checkForm()){
-            if (this.props.signupMethod === "google.com"){
-                
+export default class SignUp extends Component {
+
+    completarReg(idPlan, authLevel) {
+        if (this.checkForm()) {
+            if (this.props.signupMethod === "google.com") {
+
                 let user = this.props.user
                 user.username = this.refs.nickText.value
                 this.props.signupUserHandler(user);
@@ -14,24 +16,24 @@ export default class SignUp extends Component{
             else if (this.props.signupMethod === "email") {
                 this.props.signupWithEmailAndPasswordHandler(this.refs.email.value, this.refs.psw.value, this.refs.nickText.value, idPlan, authLevel);
             }
-            else{
+            else {
                 alert('SignUp.js - ERROR');
             }
-            
+
         }
-        else{
+        else {
             alert('ERROR - SignUp.js - completarReg');
         }
     }
 
-    checkForm(){
-        if(!(this.props.signupMethod === 'google.com')){
-            
+    checkForm() {
+        if (!(this.props.signupMethod === 'google.com')) {
+
             //Registro por email
             let txtEmail = this.refs.email.value;
             let mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/;
 
-            if(!(txtEmail.replace(/\s/g,'') !== "") &&(txtEmail.match(mailformat))){
+            if (!(txtEmail.replace(/\s/g, '') !== "") && (txtEmail.match(mailformat))) {
                 alert("Introduce un email valido!");
                 return false;
             }
@@ -39,32 +41,32 @@ export default class SignUp extends Component{
             let txtPas = this.refs.psw.value;
             let txtPasRe = this.refs.pswRepeat.value;
 
-            if(txtPas.replace(/\s/g,'') !== ""){
-                
+            if (txtPas.replace(/\s/g, '') !== "") {
+
                 //Chequeamos que no sea solo blancos
-                if(txtPas === txtPasRe){
-                    
+                if (txtPas === txtPasRe) {
+
                     //Claves iguales?
-                    if((txtPas.legth < 6)){
-                        
+                    if ((txtPas.legth < 6)) {
+
                         //No es mayor a 6
                         alert("La clave debe ser de mayor longitud (min. 6char)");
                         return false;
                     }
-                }else{
+                } else {
                     alert("Ambas claves deben ser iguales!");
                     return false;
                 }
-            }else{
+            } else {
                 alert("Introduce una clave valida!");
                 return false;
-            }   
+            }
         }
-        
+
         //Verificamos nick
         let txtName = this.refs.nickText.value;
 
-        if(txtName === ""){
+        if (txtName === "") {
             //No ha sido introducido
             alert("Introduce un nick!");
             return false;
@@ -73,71 +75,99 @@ export default class SignUp extends Component{
         return true;
     }
 
-    render(){
-        return(
-            <div> 
+    render() {
+        return (
+            <div>
+                <br />
+                {/* NAVBAR ACTIVO AQUI??*/}
+                <br />
+                <Grid verticalAlign='middle'>
+                    <Grid.Row centered>
+                        <Grid.Column computer={10} mobile={14} tablet={12}>
+                            {this.props.signupMethod !== 'google.com' &&
+                                //Si la forma de login es diferente a google para registrar
+                                //Mostrar esto
+                                <div>
+                                    <Form>
+                                        <Form.Field>
+                                            <label>Email</label>
+                                            <input type="text" placeholder="Enter Email" ref="email" required />
+                                        </Form.Field>
 
-                    { this.props.signupMethod !== 'google.com' &&
-                        //Si la forma de login es diferente a google para registrar
-                        //Mostrar esto
-                        <div>
-                            <form>
-                                <label><b>Email</b></label>
-                                <input type="text" placeholder="Enter Email" ref="email" required />
-                            
-                                <label><b>Password</b> (6 caracteres min.)</label>
-                                <input type="password" placeholder="Enter Password" ref="psw" required />
-                            
-                                <label><b>Repeat Password</b></label>
-                                <input type="password" placeholder="Repeat Password" ref="pswRepeat" required />
-                            </form>
-                        </div>  
-                    }
+                                        <Form.Field>
+                                            <label>Password (6 caracteres min.)</label>
+                                            <input type="password" placeholder="Enter Password" ref="psw" required />
+                                        </Form.Field>
 
-                    <br/>
-                    <label><b>Apodo/Nick:</b></label>
-                    <br/>
-                    <input className='iNick' maxLength="13" type='text' placeholder='Introduzca nick' ref='nickText'required />
-                    <br/>
-                    <br/>
+                                        <Form.Field>
+                                            <label>Repeat Password</label>
+                                            <input type="password" placeholder="Repeat Password" ref="pswRepeat" required />
+                                        </Form.Field>
 
-                    <div className='plans'>
-                            <div className="columns">
-                            <ul className="price">
-                                <li className="header">Basico</li>
-                                <li className="grey">Gratis</li>
-                                <li>Id plan: 1</li>
-                                <li>Nivel de acceso: 1</li>
-                                <li>10GB</li>
-                                <li className="grey"><a href="#" onClick={() =>{this.completarReg(1,1)}} className="button">Seleccionar</a></li>
-                            </ul>
-                            </div>
-    
-                            <div className="columns">
-                            <ul className="price">
-                                <li className="header">Pro</li>
-                                <li className="grey">Bs.F 2.99 / mes</li>
-                                <li>Id plan: 2</li>
-                                <li>Nivel de acceso: 3</li>
-                                <li>100GB</li>
-                                <li className="grey"><a href="#" onClick={() =>{this.completarReg(2,3)}} className="button">Seleccionar</a></li>
-                            </ul>
-                            </div>
-    
-                            <div className="columns">
-                            <ul className="price">
-                                <li className="header">Premium</li>
-                                <li className="grey">Bs.S 10 / año</li>
-                                <li>Id plan: 3</li>
-                                <li>Nivel de acceso: 5</li>
-                                <li>10TB</li>
-                                <li className="grey"><a href="#" onClick={() =>{this.completarReg(3,5)}} className="button">Seleccionar</a></li>
-                            </ul>
-                            </div>
-                    </div>
-                    
+                                    </Form>
+                                </div>
+                            }
+
+                            <br />
+                            <Form>
+                                <Form.Field>
+                                    <label>Apodo/Nick:</label>
+                                    <input className='iNick' maxLength="13" type='text' placeholder='Introduzca nick' ref='nickText' required />
+                                </Form.Field>
+                                <br />
+                            </Form>
+                            <br />
+
+                            <Card.Group centered itemsPerRow={3}>
+                                <Card>
+                                    <Card.Content>
+                                        <Card.Header>Basico</Card.Header>
+                                        <Card.Description>Gratis</Card.Description>
+                                        <List as='ul'>
+                                            <List.Item ><Icon name='right triangle'/>Id plan: 1</List.Item>
+                                            <List.Item ><Icon name='right triangle'/>Nivel de acceso: 1</List.Item>
+                                            <List.Item ><Icon name='right triangle'/>10GB</List.Item>
+                                        </List>
+                                    </Card.Content>
+                                    <Card.Content extra className='ui two buttons'>
+                                        <Button basic color='green' onClick={() => { this.completarReg(1, 1) }}>Seleccionar</Button>
+                                    </Card.Content>
+                                </Card>
+                                <Card>
+                                    <Card.Content>
+                                        <Card.Header>Pro</Card.Header>
+                                        <Card.Description>$1.99</Card.Description>
+                                        <List as='ul'>
+                                            <List.Item ><Icon name='right triangle'/>Id plan: 2</List.Item>
+                                            <List.Item ><Icon name='right triangle'/>Nivel de acceso: 3</List.Item>
+                                            <List.Item ><Icon name='right triangle'/>50GB</List.Item>
+                                        </List>
+                                    </Card.Content>
+                                    <Card.Content extra className='ui two buttons'>
+                                        <Button basic color='green' onClick={() => { this.completarReg(2, 3) }}>Seleccionar</Button>
+                                    </Card.Content>
+                                </Card>
+
+                                <Card>
+                                    <Card.Content>
+                                        <Card.Header>Ultra</Card.Header>
+                                        <Card.Description>$4.99</Card.Description>
+                                        <List as='ul'>
+                                            <List.Item ><Icon name='right triangle'/>Id plan: 2</List.Item>
+                                            <List.Item ><Icon name='right triangle'/>Nivel de acceso: 5</List.Item>
+                                            <List.Item ><Icon name='right triangle'/>150GB</List.Item>
+                                        </List>
+                                    </Card.Content>
+                                    <Card.Content extra className='ui two buttons'>
+                                        <Button basic color='green' onClick={() => { this.completarReg(3, 5) }}>Seleccionar</Button>
+                                    </Card.Content>
+                                </Card>
+                            </Card.Group>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
-        ) 
+        )
 
     }
 }
