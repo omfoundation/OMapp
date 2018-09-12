01a0f26e3c1b84bb4c2c91930f8c2a3ae5941388
lamorin <luis.morin@gmail.com>
2018-09-11 22:13:24 -0400

    .

---
+++ src/omapp/__test__/tdd.test.js
@@ -0,0 +1,9 @@
+import * as omapp from '../omapp'
+import {auth, db} from '../../constants'
+import { mockDb, mockAuth, mockUsers } from './mock-constants'
+import { User } from '../model'
+
+describe('TDD', () =>{
+
+   
+})
