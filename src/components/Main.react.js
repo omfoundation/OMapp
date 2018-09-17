984daad57ec3f419fe27666ddaea5f625f71fd5e
Madot <migueldeolim1@gmail.com>
2018-09-17 10:06:34 -0400

    fixing connect

--- src/components/Main.react.js
+++ src/components/Main.react.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Container } from 'semantic-ui-react';
+import React, { Component }from 'react';
 
 import Home from './Home.react';
 import NavBar from './NavBar.react';
 import Loading from './Loading.react';
 
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
 import {showScreenTest} from '../actions'
 
-export default class Main extends React.Component {
+export class Main extends React.Component {
     constructor(props){
         super(props);
     };
@@ -57,20 +59,19 @@ export default class Main extends React.Component {
         let {screenName} = this.props
         let screenTest;
 
+        
+
         switch (screenName) {
             case "Home":   
-                console.log('set Home');
                 screenTest = <Home user={this.props.user}
                             defaultProfilePhotoURL={this.props.defaultProfilePhotoURL}/>
             break;
 
             case "Feed":
-            console.log('set feed');
                 screenTest = <div><h2>Feed test</h2></div>
             break;
         
             default:
-                console.log('set',screenName);
                 screenTest = <div><h2>{screenName} View test</h2></div>
             break;
         }
@@ -82,3 +83,19 @@ export default class Main extends React.Component {
         )  
     }
 }
+
+function mapStateToProps(state) {
+
+    const { root } = state
+    const { screenName } = root
+
+    return {
+        screenName
+    }
+} 
+
+Main.propTypes = {
+    dispatch: PropTypes.func.isRequired
+}
+
+export default connect(mapStateToProps)(Main)
